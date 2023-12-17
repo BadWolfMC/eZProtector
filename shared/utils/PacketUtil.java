1c98b8215d09d4772a419bacf085dc0cb03f1c92
mercurialmusic <mercurialmusic@gmail.com>
2023-12-17 06:31:58 -0500

    Create PacketUtil.java

---
+++ shared/utils/PacketUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 DoNotSpamPls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.donotspampls.ezprotector.shared.utils;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+@SuppressWarnings("UnstableApiUsage")
+public class PacketUtil {
+
+  public static byte[] createSchematicaPacket() {
+    ByteArrayDataOutput output = ByteStreams.newDataOutput();
+
+    output.writeByte(0);
+    output.writeBoolean(false);
+    output.writeBoolean(false);
+    output.writeBoolean(false);
+    return output.toByteArray();
+  }
+
+  public static byte[] createWDLPacket0() {
+    ByteArrayDataOutput output = ByteStreams.newDataOutput();
+
+    output.writeInt(0);
+    output.writeBoolean(false);
+    return output.toByteArray();
+  }
+
+  public static byte[] createWDLPacket1() {
+    ByteArrayDataOutput output = ByteStreams.newDataOutput();
+
+    output.writeInt(1);
+    output.writeBoolean(false);
+    output.writeInt(0);
+    output.writeBoolean(false);
+    output.writeBoolean(false);
+    output.writeBoolean(false);
+    output.writeBoolean(false);
+    return output.toByteArray();
+  }
+}
