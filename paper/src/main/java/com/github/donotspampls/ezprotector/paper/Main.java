e18506baecb04dd1934b2cc4c043ab3d6d2a9de0
mercurialmusic <21208123+mercurialmusic@users.noreply.github.com>
2026-05-19 00:02:51 -0400

    Hopeful Regex Fix

--- paper/src/main/java/com/github/donotspampls/ezprotector/paper/Main.java
+++ paper/src/main/java/com/github/donotspampls/ezprotector/paper/Main.java
@@ -36,8 +36,8 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if (!getServer().getBukkitVersion().matches("1\\.(2[1-9]|[3-9][0-9])(\\.\\d+)?-R0\\.1-SNAPSHOT")) {
-            getLogger().severe("eZProtector is not supported on versions below 1.21!");
+        if (!getServer().getBukkitVersion().matches("(26\\.(1\\.(?:[2-9]|[1-9][0-9]+)|(?:[2-9]|[1-9][0-9]+)\\.\\d+)|(?:2[7-9]|[3-9][0-9])\\.\\d+\\.\\d+)(?:[-.].*)?")) {
+            getLogger().severe("eZProtector is not supported on versions below 26.1.2!");
             getServer().getPluginManager().disablePlugin(this);
         } else {
             saveDefaultConfig();
